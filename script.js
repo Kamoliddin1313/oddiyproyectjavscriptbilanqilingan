8e97079ba0fe9b15e9c8ef471601a7bf6e64e30c
Kamoliddin <fgdgdytdd5y>
2023-08-01 17:09:28 +0500

    oddiy proyect

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const menuopenbtn=document.querySelector("#menuopenbtn")
+const navbarpagebtn=document.querySelector(".navbarpagebtn")
+menuopenbtn.addEventListener("click",()=>{
+    navbarpagebtn.classList.toggle("activ") 
+})
